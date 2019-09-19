f9350dac5521996187b85fa78bdf90efa08de5b9
Clyde D'Souza <clyd94@gmail.com>
2019-09-19 22:30:41 +1200

    Add model with player object

---
+++ src/javascript/model/game.model.ts
@@ -0,0 +1,11 @@
+export class Player {
+    Name: string;
+    StarPlayer: boolean;
+    BattingStyle: string;
+    BowlingStyle: string;
+    Country: string;
+    Age: number;
+    BattingEconomy: number;
+    BowlingEconomy: number;
+    Id: number;
+}
