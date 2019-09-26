5002d8857df64aa389f265dd0dc387b5550a6e46
Clyde D'Souza <clyd94@gmail.com>
2019-09-26 22:39:22 +1200

    Add test case for random players

--- test/engine/gameplay.engine.test.ts
+++ test/engine/gameplay.engine.test.ts
@@ -1,4 +1,5 @@
 import { GameplayEngine } from "../../src/javascript/engine/gameplay.engine";
+import { ScorecardPlayer } from "../../src/javascript/model/game.model";
 
 describe("getCurrentOver", function ():void {
     test("should return expected results", function ():void {
@@ -37,5 +38,23 @@ describe("getTargetScore", function ():void {
         targetScore = gameplayEngine.getTargetScore(possibleRuns, 10);
         expect(targetScore).toBeGreaterThanOrEqual(50);
         expect(targetScore).toBeLessThanOrEqual(125);
+    });
+});
+
+describe("getRandomPlayers", function ():void {
+    test("should return expected results", function ():void {
+        let gameplayEngine: GameplayEngine = new GameplayEngine();
+        let players: Array<ScorecardPlayer> = [
+            new ScorecardPlayer(253802,"Virat Kohli","India",31,"Right-hand bat","Right-arm medium",10843,6.22,true,false),
+            new ScorecardPlayer(28081,"MS Dhoni","India",38,"Right-hand bat","Right-arm medium",10500,5.16,true,false),
+            new ScorecardPlayer(36084,"Yuvraj Singh","India",38,"Left-hand bat","Slow left-arm orthodox",8701,5.1,true,false),
+            new ScorecardPlayer(38699,"Ross Taylor","New Zealand",35,"Right-hand bat","Right-arm offbreak",8026,5,true,false),
+            new ScorecardPlayer(219889,"David Warner","Australia",33,"Left-hand bat","Legbreak",4343,8,false,false)
+        ];
+
+        let team: Array<ScorecardPlayer> = gameplayEngine.getRandomPlayers(players, 2);
+        expect(team.length).toEqual(2);
+        expect(team[0].id !== team[1].id).toBeTruthy();
+        expect(team[0].name !== team[1].name).toBeTruthy();
     });
 });
