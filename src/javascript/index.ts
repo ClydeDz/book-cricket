93c25fb65a1a15cf6aa78f6daf128faabd0b0c54
Clyde D'Souza <clyd94@gmail.com>
2019-09-29 20:28:01 +1300

    Remove console buttons

--- src/javascript/index.ts
+++ src/javascript/index.ts
@@ -33,44 +33,4 @@ $(document).ready(function():void {
         cpuScorecard = scorecardAPI.initCPUScorecard(allPlayers.splice(0, gameConstant.teamSize));
         userScorecard = scorecardAPI.initPlayerScorecard(allPlayers, cpuScorecard);
     });
-
-
-
-    $("#flipBtn").click(function():void {
-        console.log("flipBtn");
-        if(gameplayAPI.isGameOver(userScorecard, cpuScorecard)) {
-            gameOverShenanigans();
-            return;
-        }
-
-        let runScored: RunScored = gameplayAPI.getRunScored();
-        console.log(">>>>>>>>>>>>", runScored, "<<<<<<<<<<<<<<");
-        cpuScorecard = scorecardAPI.updateCPUScorecard(cpuScorecard, runScored.actual, userScorecard);
-        userScorecard = scorecardAPI.updatePlayerScorecard(userScorecard, cpuScorecard, runScored.actual);
-
-        if(gameplayAPI.isGameOver(userScorecard, cpuScorecard)) {
-            gameOverShenanigans();
-            return;
-        }
-    });
-
-    $("#printScorecardBtn").click(function():void {
-        printScorecard();
-    });
-
-    function printScorecard(): void {
-        console.log("**********SCORECARD*********");
-        console.log("PLAYER");
-        console.log(userScorecard);
-        console.log("CPU");
-        console.log(cpuScorecard);
-        console.log("****************************");
-    }
-
-    function gameOverShenanigans(): void {
-        let gameResultsMessage: string = gameplayAPI.didPlayerWin(userScorecard, cpuScorecard) ?
-            "CONGRATULATIONS!!! You won": "Sorry, try again";
-        console.log(gameResultsMessage);
-        printScorecard();
-    }
 });
