eb7524b580c36568dcf03a4bb4f700d82aeb8135
Clyde D'Souza <clyd94@gmail.com>
2019-09-30 20:50:25 +1300

    Add background image to the app
    Update webpack configuration to copy image to the dist folder appropriately

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
                     {
                         loader: "file-loader",
                         options: {
-                            outputPath: 'images'
+                            name: 'images/[name].[ext]'
                         }
                     }
                 ]
