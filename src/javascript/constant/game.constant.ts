532420dab45330d6d4a6ed24bca1fc3b4b9511ac
Clyde D'Souza <clyd94@gmail.com>
2019-10-06 23:31:17 +1300

    Add more delay to the stats slider rotation

--- src/javascript/constant/game.constant.ts
+++ src/javascript/constant/game.constant.ts
@@ -5,7 +5,7 @@ export class GameConstant {
     readonly possibleRuns: number[] = [0, 2, 4, 6, 8];
     readonly bookStartingPageNumber: number = 0;
     readonly bookEndingPageNumber: number = 99; // This will be suffixed with a random number from possible runs
-    readonly statsRotationTimer: number = 3000; // In milliseconds
+    readonly statsRotationTimer: number = 5000; // In milliseconds
     readonly gameResultsTimer: number = 3000; // In milliseconds
     readonly statsWicketNotation: string = "W"; 
 }
