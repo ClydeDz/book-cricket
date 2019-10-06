0ccd3560452832bf98cb8c691eb7421da0740b43
Clyde D'Souza <clyd94@gmail.com>
2019-10-06 23:30:07 +1300

    Update total balls setting to 5 overs or 30 balls

--- src/javascript/constant/game.constant.ts
+++ src/javascript/constant/game.constant.ts
@@ -1,7 +1,7 @@
 export class GameConstant {
     readonly teamSize: number = 5;
     readonly numberOfTeams: number = 2;
-    readonly totalBalls: number = 6;
+    readonly totalBalls: number = 30;
     readonly possibleRuns: number[] = [0, 2, 4, 6, 8];
     readonly bookStartingPageNumber: number = 0;
     readonly bookEndingPageNumber: number = 99; // This will be suffixed with a random number from possible runs
